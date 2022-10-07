2270cbcbf77d12ace2cb45dddc62650a2c68cc94
Huylenbroeck Florent <flhu@odoo.com>
2022-10-07 10:07:37 +0200

    Update .add-version.sh

--- .add-version.sh
+++ .add-version.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Check for $MULTIVERSEPATH in ~/.bashrc
-if [ -z ${MULTIVERSEPATH+x} ]; then
-    echo "\$MULTIVERSEPATH not found in $HOME/.bashrc, please append \"export \$MULTIVERSEPATH=<path-to-src>\" to $HOME/.bashrc and run 'source $HOME/.bashrc'"
-    exit 1
-fi
-
 # Check args
 if [ "$#" -lt 1  ]; then
     echo ".add-version: Add Odoo <versions>... (space-separated list of version name) to multiverse."
     echo ""
     echo "Usage: .add-version <versions>..."
+    exit 1
+fi
+
+# Check for $MULTIVERSEPATH in ~/.bashrc
+if [ -z ${MULTIVERSEPATH+x} ]; then
+    echo "\$MULTIVERSEPATH not found in $HOME/.bashrc, please append \"export \$MULTIVERSEPATH=<path-to-src>\" to $HOME/.bashrc and run 'source $HOME/.bashrc'"
     exit 1
 fi
 
