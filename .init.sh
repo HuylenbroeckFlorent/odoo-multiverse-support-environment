5a2c5d4828d14e70fa94e28e4a80f24b1e8583b1
HuylenbroeckFlorent <huylenbroeck.florent@gmail.com>
2023-02-16 11:16:21 +0100

    split .init in two files with different use

--- .init.sh
+++ .init.sh
@@ -7,36 +7,6 @@ if ! [[ "$(pwd)" =~ $home_matching ]]; then
 	exit 1
 fi
 
-# Disclaimer
-echo "Some command will require root privilege or user interaction (installing git, psql or python)"
-
-# Check for git installation
-git_matching="git version.*$"
-if ! [[ "$(git --version)" =~ $git_matching ]]; then
-	sudo apt install git
-fi
-
-# Checking for python installation
-py3_matching="Python 3.*$"
-if ! [[ "$(python3 --version)" =~ $py3_matching ]]; then
-	sudo apt install python3
-	sudo apt install python3-pip
-fi
-
-# Checking for psql installation
-psql_matching="psql \(PostgreSQL\).*$"
-if ! [[ "$(psql --version)" =~ $psql_matching ]]; then
-	sudo apt install postgresql postgresql-client
-fi
-# Checking for psql user
-if ! [[ "$(psql postgres -tAc "SELECT * FROM pg_roles WHERE rolname='$USER'")" =~ ^"$USER"\|t\|t\|(t|f|)\|t\|t\|(t|f|)\|(-?[0-9]*|)\|(\**|)\|(t|f|)\|(t|f|)\|(t|f|)\|([0-9]*|)$ ]]; then
-	sudo -u postgres psql -U postgres -c "CREATE USER $USER;"
-	sudo -u postgres psql -U postgres -c "ALTER USER $USER SUPERUSER CREATEDB INHERIT LOGIN"
-	createdb "$USER"
-fi
-# Checking for trial psql user
-psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='trial'" | grep -q 1 || sudo -u postgres psql -U postgres -c "CREATE USER trial;"
-
 # Paths
 odoohome=$(pwd)
 worktreesrc="$odoohome/src/master"
@@ -96,6 +66,7 @@ do
 	fi
 done
 
+# Exporting to ~/.bashrc
 sed -i "/export ODOOHOME=.*/d" $HOME/.bashrc
 sed -i '/source.*multiverserc/d' $HOME/.bashrc
 echo "export ODOOHOME=$odoohome" >> $HOME/.bashrc
---
+++ .prereqs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Disclaimer
+echo "Some command will require root privilege or user interaction (installing git, psql or python)"
+
+# Check for git installation
+git_matching="git version.*$"
+if ! [[ "$(git --version)" =~ $git_matching ]]; then
+	sudo apt install git
+fi
+
+# Checking for python installation
+py3_matching="Python 3.*$"
+if ! [[ "$(python3 --version)" =~ $py3_matching ]]; then
+	sudo apt install python3
+	sudo apt install python3-pip
+fi
+
+# Checking for psql installation
+psql_matching="psql \(PostgreSQL\).*$"
+if ! [[ "$(psql --version)" =~ $psql_matching ]]; then
+	sudo apt install postgresql postgresql-client
+fi
+
+# Checking for psql user and its roles
+if ! [[ "$(psql postgres -tAc "SELECT * FROM pg_roles WHERE rolname='$USER'")" =~ ^"$USER"\|t\|t\|(t|f|)\|t\|t\|(t|f|)\|(-?[0-9]*|)\|(\**|)\|(t|f|)\|(t|f|)\|(t|f|)\|([0-9]*|)$ ]]; then
+	sudo -u postgres psql -U postgres -c "CREATE USER $USER;"
+	sudo -u postgres psql -U postgres -c "ALTER USER $USER SUPERUSER CREATEDB INHERIT LOGIN"
+	createdb "$USER"
+fi
+
+# Checking for trial psql user
+psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='trial'" | grep -q 1 || sudo -u postgres psql -U postgres -c "CREATE USER trial;"
