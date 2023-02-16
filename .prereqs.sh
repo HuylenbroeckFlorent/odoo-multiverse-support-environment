c58b50555352954db4b8d01c242bce9b79b306bf
HuylenbroeckFlorent <huylenbroeck.florent@gmail.com>
2023-02-16 11:36:05 +0100

    Made prereqs execution less verbose when requirements are already satisfied

--- .prereqs.sh
+++ .prereqs.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Disclaimer
-echo "Some command will require root privilege or user interaction (installing git, psql or python)"
+echo "Starting"
+echo "NOTE : some command might require root privilege or user interaction (installing git, psql or python)"
 
 # Check for git installation
+echo -e "\t Checking Git installation"
 git_matching="git version.*$"
 if ! [[ "$(git --version)" =~ $git_matching ]]; then
 	sudo apt install git
 fi
 
 # Checking for python installation
+echo -e "\t Checking python installation"
 py3_matching="Python 3.*$"
 if ! [[ "$(python3 --version)" =~ $py3_matching ]]; then
 	sudo apt install python3
@@ -17,6 +20,7 @@ if ! [[ "$(python3 --version)" =~ $py3_matching ]]; then
 fi
 
 # Checking for psql installation
+echo -e "\t Checking PSQL installation and users"
 psql_matching="psql \(PostgreSQL\).*$"
 if ! [[ "$(psql --version)" =~ $psql_matching ]]; then
 	sudo apt install postgresql postgresql-client
@@ -31,3 +35,5 @@ fi
 
 # Checking for trial psql user
 psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='trial'" | grep -q 1 || sudo -u postgres psql -U postgres -c "CREATE USER trial;"
+
+echo "Done"
