66465ef98272204ccb8991874ee2e8f1deb090fe
HuylenbroeckFlorent <flhu@odoo.com>
2024-06-06 15:58:11 +0200

    added support for odoofin server in oe-odoo-bin

--- utils/.odoo-bin.sh
+++ utils/.odoo-bin.sh
@@ -6,10 +6,14 @@ odoo-bin-help() {
     echo "DESCRIPTION"
     echo -e "\t From outside of any '$MULTIVERSEPATH/*/odoo/' directory, launches odoo-bin from"
     echo -e "\t the chosen version. From inside any '$MULTIVERSEPATH/*/odoo/ directory', launches"
-    echo -e "\t that specific version of odoo-bin."
+    echo -e "\t that specific version of odoo-bin. Version 'odoofin' can also be used to launch"
+    echo -e "\t an odooFin server."
     echo "SYNOPSIS:"
     echo -e '\t odoo-bin <version> [--debug] [args...]'
     echo -e "\t\t Launches 'version' version of odoo-bin with args as parameters."
+    echo
+    echo -e '\t odoo-bin odoofin [--debug] [args...]'
+    echo -e "\t\t Launches an odoofin server with args as parameters."
     echo
     echo -e '\t odoo-bin [--debug] [args...]'
     echo -e "\t\t Launches the version of odoo-bin in the current directory with args as parameters."
@@ -31,8 +35,13 @@ if [[ $# -gt 0 ]] && [[ $1 =~ ^("-h"|"--help")$ ]]; then
     exit 0
 fi
 
+odoofin=false
 if [[ $# -gt 0 ]] && [[ $1 =~ (master|saas-[0-9]+.[1-4]|[0-9]+.0) ]] && [[ "$(ls $MULTIVERSEPATH)" =~ ^.*"$1".*$ ]]; then
     version="$1"
+    shift
+elif [[ $# -gt 0 ]] && [[ $1 =~ odoofin ]]; then
+    odoofin=true
+    version="17.0"
     shift
 elif [[ "$(pwd)" =~ "$MULTIVERSEPATH"/(master|saas-[0-9]+.[1-4]|[0-9]+.0)/odoo ]]; then
     version=$(pwd)
@@ -44,11 +53,12 @@ else
     exit 1
 fi
 
-# Check for --debug flag
+# Check for flags
+# --debug  to start a debugpy environment
 debug=false
 for arg in $@
 do
-    shift 
+    shift
     if [[ "$arg" = "--debug" ]]; then 
         debug=true 
         continue
@@ -56,12 +66,39 @@ do
     set -- "$@" "$arg"
 done
 
-# Launch command 
-if [[ debug ]]; then
-    pip3 list | grep -q "debugpy" || (echo "Debugpy not found. Installing..." && pip3 install debugpy >/dev/null) 2>/dev/null
-    python3 -m debugpy --listen localhost:5678 $MULTIVERSEPATH/$version/odoo/odoo-bin $@ "--addons-path=$MULTIVERSEPATH/$version/odoo/addons,$MULTIVERSEPATH/$version/enterprise,$MULTIVERSEPATH/$version/design-themes" --max-cron-threads=0
-else
-    $MULTIVERSEPATH/$version/odoo/odoo-bin $@ "--addons-path=$MULTIVERSEPATH/$version/odoo/addons,$MULTIVERSEPATH/$version/enterprise,$MULTIVERSEPATH/$version/design-themes" --max-cron-threads=0
+# Build arguments
+addonspath="--addons-path=\
+$MULTIVERSEPATH/$version/odoo/addons,\
+$MULTIVERSEPATH/$version/enterprise,\
+$MULTIVERSEPATH/$version/design-themes,\
+$ODOOHOME/internal/default,\
+$ODOOHOME/internal/trial"
+
+upgradepath="--upgrade-path=\
+$MULTIVERSEPATH/master/upgrade-util/src,\
+$MULTIVERSEPATH/master/upgrade/migrations"
+
+args=$@
+
+if [ "$odoofin" = true ]; then
+    addonspath="${addonspath},$ODOOHOME/odoofin"
+    args="${args} --unaccent --http-port 6969"
+    export REQUESTS_CA_BUNDLE=/etc/ssl/certs/nginx-selfsigned.crt
 fi
 
-# (',$ODOOHOME/internal/default,$ODOOHOME/internal/trial' could be added to addons path)
+# Build command
+commandline="$MULTIVERSEPATH/$version/odoo/odoo-bin $args $addonspath $upgradepath --max-cron-threads=0"
+
+if [ "$debug" = true ]; then
+    pip3 list | grep -q "debugpy" || (echo "Debugpy not found. Installing..." && pip3 install debugpy >/dev/null) 2>/dev/null
+    commandline="python3 -m debugpy --listen localhost:5678 ${commandline}"
+fi
+
+# Launch command
+echo -e "Running the following command\n\t$commandline"
+eval $commandline
+
+# Cleanup after execution
+if [ "$odoofin" = true ]; then
+    unset REQUESTS_CA_BUNDLE
+fi
